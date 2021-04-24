471e7c1b5e215250c68cef6bd0c007dca8ed67c5
Andrii <korposh98@gmail.com>
2021-04-24 15:21:11 +0300

    Add custom product attribute

---
+++ Block/Product/View/GeographicAvailability.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Speroteck\ProductOption\Block\Product\View;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Element\Template\Context;
+use Magento\Framework\Registry;
+
+/**
+ * Class GeographicAvailability
+ * @package Speroteck\ProductOption\Block\Product\View
+ */
+class GeographicAvailability extends Template
+{
+
+    /**
+     * @var Registry
+     */
+    protected $registry;
+
+    /**
+     * GeographicAvailability constructor.
+     * @param Context $context
+     * @param Registry $registry
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        Registry $registry,
+        array $data = []
+    ) {
+        $this->registry = $registry;
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * @return string
+     */
+    public function getGeographicAvailability()
+    {
+        /** @var \Magento\Catalog\Model\Product $currentProduct */
+        $currentProduct = $this->registry->registry('current_product');
+        if ($currentProduct && $currentProduct->getId()) {
+            return $currentProduct->getGeographicAvailability();
+        }
+        return '';
+    }
+}
---
+++ Setup/InstallData.php
@@ -0,0 +1,59 @@
+<?php
+namespace Speroteck\ProductOption\Setup;
+
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+class InstallData implements InstallDataInterface
+{
+    /**
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     * InstallData constructor.
+     * @param EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(EavSetupFactory $eavSetupFactory)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * @param ModuleDataSetupInterface $setup
+     * @param ModuleContextInterface $context
+     */
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+        $eavSetup->addAttribute(
+            \Magento\Catalog\Model\Product::ENTITY,
+            'geographic_availability',
+            [
+                'type' => 'text',
+                'backend' => '',
+                'frontend' => '',
+                'label' => 'Geographic Availability',
+                'input' => 'text',
+                'class' => '',
+                'source' => '',
+                'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_GLOBAL,
+                'visible' => true,
+                'required' => false,
+                'user_defined' => false,
+                'default' => '',
+                'searchable' => false,
+                'filterable' => false,
+                'comparable' => false,
+                'visible_on_front' => false,
+                'used_in_product_listing' => true,
+                'unique' => false,
+                'apply_to' => ''
+            ]
+        );
+    }
+}
